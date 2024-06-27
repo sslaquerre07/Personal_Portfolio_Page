69baead5eb5865f6fc9f5cacc436136c9ede9a22
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-06-26 22:39:23 -0600

    Border set up, certificates to be created later

--- portfolio_page/src/component/Certifications.js
+++ portfolio_page/src/component/Certifications.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 function certifications() {
     return(
-        <div>
+        <div className='page'>
+            <Header />
+            <div className="body vertical">
+                <div id='certification-wall'>
+                </div>
+                <div id='skill-list'>
 
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
