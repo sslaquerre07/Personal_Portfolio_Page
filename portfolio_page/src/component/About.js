f52e38e535d431ce2ac79e648715d8bd81229b61
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-06-26 19:14:58 -0600

    Most styling choices complete

--- portfolio_page/src/component/About.js
+++ portfolio_page/src/component/About.js
@@ -12,26 +12,42 @@ const pageInfo = {
     life: {
         title: "My Life",
         images: [Life1, Life2],
-        text: [
-            "hello",
-            ""
-        ]
+        text: 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+        dolore magna aliqua. Bibendum arcu vitae elementum curabitur vitae. Enim praesent elementum facilisis 
+        leo vel. Dictumst quisque sagittis purus sit. At tellus at urna condimentum. Adipiscing commodo elit 
+        at imperdiet. Posuere morbi leo urna molestie at. Laoreet sit amet cursus sit amet. Accumsan sit amet 
+        nulla facilisi morbi tempus iaculis urna. Amet nulla facilisi morbi tempus iaculis urna id volutpat. 
+        <br /><br />Blandit turpis cursus in hac habitasse. Interdum varius sit amet mattis vulputate enim nulla. Sed 
+        ullamcorper morbi tincidunt ornare massa eget egestas purus. Sagittis purus sit amet volutpat consequat 
+        mauris. Volutpat lacus laoreet non curabitur gravida. Mi ipsum faucibus vitae aliquet nec ullamcorper sit.</p>
     },
     coding: {
         title: "My journey into Software Engineering",
         images: [Code1, Code2],
-        text: [
-            "",
-            ""
-        ]
+        text: 
+            <p>Believe it or not, I had no prior coding experience coming into university. My first exposure 
+            came in our introductory programming class in my first semester. Immediately, I fell in love with the
+            world of software development. What fascinated me the most was the endless world of
+            constantly changing information to learn. One of the main things I was looking for in a career was the
+            potential for continuous learning. In that regard, software engineering is one of the best careers out there.
+            <br /><br />Following the discovery of my passion, there was no doubt what my major
+            declaration would be at the end of the year. I have been thrilled with my decision, and look forward
+            to a career in one of the most innovative industries in the world.</p>
+        
     },
     golf: {
         title: "Another one of my many passions: Golf!",
         images: [Golf1, Golf2],
-        text: [
-            "",
-            ""
-        ]
+        text: 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Bibendum arcu vitae elementum curabitur vitae. Enim praesent elementum facilisis 
+                leo vel. Dictumst quisque sagittis purus sit. At tellus at urna condimentum. Adipiscing commodo elit 
+                at imperdiet. Posuere morbi leo urna molestie at. Laoreet sit amet cursus sit amet. Accumsan sit amet 
+                nulla facilisi morbi tempus iaculis urna. Amet nulla facilisi morbi tempus iaculis urna id volutpat. 
+                <br /><br />Blandit turpis cursus in hac habitasse. Interdum varius sit amet mattis vulputate enim nulla. Sed 
+                ullamcorper morbi tincidunt ornare massa eget egestas purus. Sagittis purus sit amet volutpat consequat 
+                mauris. Volutpat lacus laoreet non curabitur gravida. Mi ipsum faucibus vitae aliquet nec ullamcorper sit.</p>
     }
 }
 
@@ -52,22 +68,21 @@ function About(){
                         <input type="radio" id="software-story" name="option" value="software-option" onClick={() => setButton("coding")}></input>
                         <label htmlFor="software-story">How I got into Software Development</label>                        
                     </div>
-                    <div id="sports">
-                        <input type="radio" id="sports-story" name="option" value="sport-option" onClick={() => setButton("golf")}></input>
-                        <label htmlFor="sports-story">Sport</label>                        
+                    <div id="golf">
+                        <input type="radio" id="golf-story" name="option" value="golf-option" onClick={() => setButton("golf")}></input>
+                        <label htmlFor="golf-story">Golf</label>                        
                     </div>
                 </div>
                 <div id="text-box">
                     <table>
                         <tr>
-                            <th id="title" colSpan={2}>{pageInfo[button]["title"]}</th>
+                            <th id="title" colSpan={3}>{pageInfo[button]["title"]}</th>
                         </tr>
                         <tr>
                             <td className="image-container"><img src={pageInfo[button].images[0]} alt="" className="image"/></td>
-                            <td className="text"><p>{pageInfo[button].text[0]}</p></td>
+                            <td className="text" colSpan={2} rowSpan={2}>{pageInfo[button].text}</td>
                         </tr>
                         <tr>
-                            <td className="text"><p>{pageInfo[button].text[1]}</p></td>
                             <td className="image-container"><img src={pageInfo[button].images[1]} alt="" className="image"/></td>
                         </tr>
                     </table>
--- portfolio_page/src/component/Footer.js
+++ portfolio_page/src/component/Footer.js
@@ -5,12 +5,11 @@ function Footer() {
         <div id="footer-container">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <div id="name">
-                <b>Sam Laquerre</b>
                 <p>Personal Portfolio</p>
             </div>
             <div id="social-section">
-                <b>Follow</b>
                 <div id="socials">
+                    <b>Follow:</b>
                     <a href="https://github.com/sslaquerre07" class="fa fa-github" />
                     <a href="https://www.linkedin.com/in/samuel-laquerre-304635202/" class="fa fa-linkedin" />
                 </div>
