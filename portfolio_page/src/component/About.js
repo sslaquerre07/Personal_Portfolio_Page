7349b7ad5394acd8c32171bd78af914a613a7898
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-07-14 10:38:25 -0600

    package page changed

--- portfolio_page/src/component/About.js
+++ portfolio_page/src/component/About.js
@@ -40,14 +40,12 @@ const pageInfo = {
         title: "Another one of my many passions: Golf!",
         images: [Golf1, Golf2],
         text: 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                dolore magna aliqua. Bibendum arcu vitae elementum curabitur vitae. Enim praesent elementum facilisis 
-                leo vel. Dictumst quisque sagittis purus sit. At tellus at urna condimentum. Adipiscing commodo elit 
-                at imperdiet. Posuere morbi leo urna molestie at. Laoreet sit amet cursus sit amet. Accumsan sit amet 
-                nulla facilisi morbi tempus iaculis urna. Amet nulla facilisi morbi tempus iaculis urna id volutpat. 
-                <br /><br />Blandit turpis cursus in hac habitasse. Interdum varius sit amet mattis vulputate enim nulla. Sed 
-                ullamcorper morbi tincidunt ornare massa eget egestas purus. Sagittis purus sit amet volutpat consequat 
-                mauris. Volutpat lacus laoreet non curabitur gravida. Mi ipsum faucibus vitae aliquet nec ullamcorper sit.</p>
+            <p>I have always been a person involved in as many sports as possible throughout 
+                my life. However, something about the game of golf captivated me, making it one of my greatest passions. 
+                The ability to escape from all problems in your personal life and spend four hours in nature 
+                is unlike any other sport. <br /><br />It is also a game of lifelong improvement, there will never be 
+                a time in which you are completely content with your skills. Similarly to being 
+                a software engineer, there will always be something to learn.</p>
     }
 }
 
