1d49a65929e9a110ec82addb3bc096c7ef14c99b
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-07-15 21:27:59 -0600

    Email backend working

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
